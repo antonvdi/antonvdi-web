cce7916f4620f38b0dd29210bc053a66c6221aec
Anton Irvold <anirv20@student.sdu.dk>
2024-08-30 11:45:15 +0200

    better streaming

--- frontend/src/components/Examples.tsx
+++ frontend/src/components/Examples.tsx
@@ -1,33 +1,44 @@
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import StreamingText from "./StreamingText";
 import { httpsCallable } from "firebase/functions";
 import { functions } from "..";
 import { useEffect, useState } from "react";
-import { IconButton } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CircularProgress,
+} from "@mui/material";
 import RefreshIcon from "@mui/icons-material/Refresh";
+import { ReactTyped } from "react-typed";
 
 export default function Examples() {
   const [prevExamples, setPrevExamples] = useState<string[]>([]);
   const [example, setExample] = useState<string>("");
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const askOpenAI = httpsCallable(functions, "askOpenAI");
 
-  const updateExample = () => {
-    askOpenAI({ prevAnswers: prevExamples })
-      .then((result) => {
-        setExample(result.data as string);
-        setPrevExamples((prevExamples) => [...prevExamples, example]);
-      })
-      .catch((error) => {
-        console.error("Error calling askOpenAI:", error);
-      });
+  const updateExample = async () => {
+    const result = await askOpenAI({ prevAnswers: prevExamples });
+    setExample(result.data as string);
+    setPrevExamples((prevExamples) => [...prevExamples, example]);
   };
 
   useEffect(() => {
     updateExample();
   }, []);
+
+  const handleClick = () => {
+    setIsDisabled(true);
+    setExample("");
+    updateExample();
+
+    setTimeout(() => {
+      setIsDisabled(false);
+    }, 5000);
+  };
 
   return (
     <Container
@@ -42,36 +53,39 @@ export default function Examples() {
         gap: { xs: 3, sm: 6 },
       }}
     >
-      <Box
-        sx={{
-          width: { sm: "100%", md: "60%" },
-          textAlign: { sm: "left", md: "center" },
-        }}
-      >
-        <Box display="flex" alignItems="center">
-          <IconButton
-            onClick={updateExample}
-            size="small"
-            sx={{ top: -5, mr: 2 }}
-          >
-            <RefreshIcon />
-          </IconButton>
+      <Card sx={{ width: 400 }}>
+        <CardContent>
           <Typography
-            component="h2"
-            variant="h4"
             gutterBottom
-            sx={{ color: "text.primary" }}
+            sx={{ color: "text.secondary", fontSize: 14 }}
+            fontStyle={"italic"}
           >
+            Genereret af AI
+          </Typography>
+          <Typography variant="h5" component="div" sx={{ mb: 2 }}>
             AI kan hjælpe dig med...
           </Typography>
-        </Box>
 
-        <StreamingText
-          variant="body1"
-          sx={{ color: "text.secondary" }}
-          message={example.toLocaleLowerCase().trim()}
-        />
-      </Box>
+          <Typography variant="body2" color="text.secondary" component="div">
+            <ReactTyped
+              strings={[example.toLocaleLowerCase().trim()]}
+              typeSpeed={40}
+            />
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            startIcon={
+              isDisabled ? <CircularProgress size={20} /> : <RefreshIcon />
+            }
+            onClick={!isDisabled ? handleClick : undefined}
+            disabled={isDisabled}
+          >
+            Regenerér
+          </Button>
+        </CardActions>
+      </Card>
     </Container>
   );
 }
